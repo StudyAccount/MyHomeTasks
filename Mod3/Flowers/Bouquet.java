99bbc59d17bcb364a0be3ab4107d3cb76472c329
Viktor <vityagorbenko@yandex.ru>
2016-03-07 02:37:47 +0200

    Added files via upload

---
+++ Mod3/Flowers/Bouquet.java
@@ -0,0 +1,18 @@
+package com.goit.gojavaonline;
+
+import java.util.List;
+
+/**
+ * Created by User on 07.03.2016.
+ */
+public class Bouquet {
+    private List<Flower> flowers;
+
+    public List<Flower> getFlowers() {
+        return flowers;
+    }
+
+    public void setFlowers(List<Flower> flowers) {
+        this.flowers = flowers;
+    }
+}
---
+++ Mod3/Flowers/Rosebush.java
@@ -0,0 +1,18 @@
+package com.goit.gojavaonline;
+
+import java.util.List;
+
+/**
+ * Created by User on 07.03.2016.
+ */
+public class Rosebush {
+    private List<Rose> roses;
+
+    public List<Rose> getRoses() {
+        return roses;
+    }
+
+    public void setRoses(List<Rose> roses) {
+        this.roses = roses;
+    }
+}
