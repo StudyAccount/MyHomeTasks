6130c2523dde0a3842defabe16f266b179a8f565
Viktor <vityagorbenko@yandex.ru>
2016-03-07 02:36:49 +0200

    Added files via upload

---
+++ Mod3/Files/Directory.java
@@ -0,0 +1,18 @@
+package com.goit.gojavaonline;
+
+import java.util.List;
+
+/**
+ * Created by User on 06.03.2016.
+ */
+public class Directory {
+    private List<File> files;
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+}
---
+++ Mod3/Files/File.java
@@ -0,0 +1,13 @@
+package com.goit.gojavaonline;
+
+import java.util.List;
+
+/**
+ * Created by User on 06.03.2016.
+ */
+public class File {
+    public static void main(String[] args) {
+
+    }
+
+}
